f7a1bc8e3c07fe3addcfdef3f06088cb34378e75
ZWJ <114372734+BigWhiteBearZWJ@users.noreply.github.com>
2024-08-20 11:48:00 +0800

    Update contrastive_regularization.py

--- code/contrastive_regularization.py
+++ code/contrastive_regularization.py
@@ -40,13 +40,6 @@ class ContrastiveLoss(AbstractLossClass):
         super().__init__()
         self.margin = margin
     
-    def contrastive_loss(self, anchor, positive, negative):
-        dist_pos = F.pairwise_distance(anchor, positive)
-        dist_neg = F.pairwise_distance(anchor, negative)
-        # Compute loss as the distance between anchor and negative minus the distance between anchor and positive
-        loss = torch.mean(torch.clamp(dist_pos - dist_neg + self.margin, min=0.0))
-        return loss
-    
     def id_contrastive_loss(self, art, id1, id2):
         dist_id = F.cosine_similarity(art, id1) + F.cosine_similarity(art, id2)
         # Compute loss as the distance between anchor and negative minus the distance between anchor and positive
@@ -68,28 +61,7 @@ class ContrastiveLoss(AbstractLossClass):
         idx_list = list(range(bs//2, bs))
         random.shuffle(idx_list)
         fake_common_anchor = common[idx_list]
-        ### specific
-        # specific_anchor = swap_spe_features(spe_label, specific)
-        # real_specific_anchor, fake_specific_anchor = specific_anchor.chunk(2)
-        # real_specific, fake_specific = specific.chunk(2)
-
-        # Compute the contrastive loss of common between real and fake
-        # loss_realcommon = self.contrastive_loss(real_common, real_common_anchor, fake_common_anchor)
-        # loss_fakecommon = self.contrastive_loss(fake_common, fake_common_anchor, real_common_anchor)
 
         loss_id = (2-self.id_contrastive_loss(real_common, real_tar, real_src)) + self.id_contrastive_loss(fake_common, fake_tar, fake_src)
-        # print("real:", self.id_contrastive_loss(real_common, real_tar, real_src))
-        # print("-----")
-        # print("fake:", self.id_contrastive_loss(fake_common, fake_tar, fake_src))
-
-        # loss_id = self.id_contrastive_loss(real_common, real_tar, real_src) + self.id_contrastive_loss(fake_common, fake_tar, fake_src)
-
-        # Comupte the constrastive loss of specific between real and fake
-        # loss_realspecific = self.contrastive_loss(real_specific, real_specific_anchor, fake_specific_anchor)
-        # loss_fakespecific = self.contrastive_loss(fake_specific, fake_specific_anchor, real_specific_anchor)
-
-        # Compute the final loss as the sum of all contrastive losses
-        # loss = loss_realcommon + loss_fakecommon + loss_fakespecific + loss_realspecific
-        # loss = loss_realcommon + loss_fakecommon + loss_id
         loss = loss_id
         return loss
